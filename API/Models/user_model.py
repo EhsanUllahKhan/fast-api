8ac3e1d4a4c6daba461c8d558195502e86855da2
EhsanUllahKhan <ullahkhanihsan@yahoo.com>
2021-03-21 15:43:15 +0500

    user iinserted to db

--- API/Models/user_model.py
+++ API/Models/user_model.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import relationship
 
 
 from pydantic import BaseModel
-
+from API.Models import lost_item_model,found_item_model
 from ..database import Base
 
 
@@ -14,8 +14,8 @@ class User(Base):
     user_id = Column(Integer, primary_key=True, index=True)
     email = Column(String(length=20), unique=True, index=True)
     password = Column(String(length=20))
-
-    lost_items = relationship("Lost_Item", backref="users")
-    found_items = relationship("Found_Item", backref="users")
-    # lost_items = relationship("Lost_Item", back_populates="users")
-    # found_items = relationship("Found_Item", back_populates="users")
+    #
+    # lost_items = relationship("lost_items", backref="users")
+    # found_items = relationship("found_items", backref="users")
+    lost_items = relationship("Lost_Item", back_populates="users")
+    found_items = relationship("Found_Item", back_populates="users")
